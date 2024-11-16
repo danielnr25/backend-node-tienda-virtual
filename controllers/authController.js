996d93459551d8f42c35449ac865ea34687c9483
danielnr25 <69592632+danielnr25@users.noreply.github.com>
2024-11-15 21:38:39 -0500

    feat:proyecto_backend_node

---
+++ controllers/authController.js
@@ -0,0 +1,28 @@
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+
+
+module.exports.login = async (req,res) =>{
+    const {username,password} = req.body;
+
+    try {
+        const results = await User.findByUsername(username);
+        if(results.length>0){
+            const user = results[0];
+            const isMatch = await bcrypt.compare(password,user.password);   
+      
+            if(isMatch){
+                return res.status(200).json({message: "Usuario autenticado con éxito"})
+            }
+        }else{
+            return res.status(401).json({message: "Usuario o contraseña incorrecta"})
+        }
+
+    } catch (error) {
+        console.error("Error inesperado:",error);
+        return res.status(500).json({message:"Error interno del servidor, comuniquese con el administrador del sistema."});
+    }
+
+
+}
+
